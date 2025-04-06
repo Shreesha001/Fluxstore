27053d578049a468fd3438c110a30f95cfa14f14
Shreesha Shetty <shettyshreesha552@gmail.com>
2025-04-06 19:59:35 +0530

    commit auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluxstore/firebase_options.dart';
+import 'package:fluxstore/responsive/mobile_screen_layout.dart';
 import 'package:fluxstore/screens/auth_screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -12,16 +14,33 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Fluxstore',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: LoginScreen(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(color: Colors.blue),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const MobileScreenLayout();
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          return const LoginScreen();
+        },
+      ),
     );
   }
 }
--- lib/screens/main_screens/profile_screen.dart
+++ lib/screens/main_screens/profile_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
+  void logoutUser(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Profile Screen")));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder:
+                    (context) => AlertDialog(
+                      title: const Text('Logout'),
+                      content: const Text('Are you sure you want to logout?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            logoutUser(context);
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            'Logout',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ],
+                    ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(child: Text("Profile Screen")),
+    );
   }
 }
